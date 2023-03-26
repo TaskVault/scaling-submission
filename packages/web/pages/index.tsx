838e18ed59db87b98438bb6eec50e2a22f096d73
Jannik <theofficialjannik@gmail.com>
2023-03-26 11:02:49 +0200

    changed code

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ConnectButton } from '@web3uikit/web3'
 
 export default function Home() {
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Task Vault</h1>
+      <ConnectButton />
     </div>
   )
 }
