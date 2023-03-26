91774362c67d731bd7c46ca6d678d8f6af4d17e3
Jannik <theofficialjannik@gmail.com>
2023-03-26 12:05:07 +0200

    added network

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -1,4 +1,20 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    polygon: {
+      url: MUMBAI_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
   solidity: "0.8.17",
 };
