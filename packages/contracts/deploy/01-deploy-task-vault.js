3605d7430bfaa5be09e13ef0a70d9c84ab6e12de
Jannik <theofficialjannik@gmail.com>
2023-03-26 12:17:21 +0200

    changed code

--- packages/contracts/deploy/01-deploy-task-vault.js
+++ packages/contracts/deploy/01-deploy-task-vault.js
@@ -4,7 +4,7 @@ const main = async () => {
     const USDC_ADDRESS_MUMBAI = "0x0FA8781a83E46826621b3BC094Ea2A0212e71B23";
     const FREELANCER_DEPOSIT = ethers.utils.parseUnits('10', 6);
 
-    const TaskVault = await ethers.getContractFactory("TaskVault");
+    const TaskVault = await ethers.getContractFactory('TaskVault');
     const taskVault = await TaskVault.deploy(USDC_ADDRESS_MUMBAI, FREELANCER_DEPOSIT);
 
     console.log("TaskVault deployed to:", taskVault.address);
